9e0591ac46c0c51d0ddcdf3c42951b18b247eaba
SimonVesely <simvesely@gmail.com>
2024-10-09 21:27:59 +0200

    Přidání inputu v hxh modu, potřeba další fix

--- Linux_MacOS/lib/hxh.c
+++ Linux_MacOS/lib/hxh.c
@@ -4,8 +4,8 @@
 
 void humanXhuman() {
   clearScreen();
-  int rows = 3, cols = 3, x = 0, y = 0;
-  char jmeno1[256], jmeno2[256], hraciPole[2][2][2],
+  int rows = 3, cols = 3, pocetTahu = 0, aktHrac = 1;
+  char jmeno1[256], jmeno2[256], hraciPole[2][2][2], choice[256],
       ukazkovePole[3][3][3] = {
           {"A1", "A2", "A3"}, {"B1", "B2", "B3"}, {"C1", "C2", "C3"}};
   printf("Zadej jméno hráč č.1: ");
@@ -13,27 +13,42 @@ void humanXhuman() {
   clearScreen();
   printf("Zadej jméno hráč č.2: ");
   scanf("%s", jmeno2);
-  clearScreen();
 
-  puts("Ukázkové pole:");
-  for (int i = 0; i < rows; i++) {
-    if (x == 0) {
-      puts("\n____________");
-      x++;
-    }
-    for (int j = 0; j < cols; j++) {
-      printf("|%s ", ukazkovePole[i][j]);
-      if ((y == 2) || (y == 5) || (y == 8)){
-        printf("|");
+  while (pocetTahu != 9) {
+    clearScreen();
+    int x = 0, y = 0;
+    puts("Ukázkové pole:");
+    for (int i = 0; i < rows; i++) {
+      if (x == 0) {
+        puts("\n____________");
+        x++;
       }
-      y++;
+      for (int j = 0; j < cols; j++) {
+        printf("|%s ", ukazkovePole[i][j]);
+        if ((y == 2) || (y == 5) || (y == 8)) {
+          printf("|");
+        }
+        y++;
+      }
+      if ((x == 1) || (x == 2)) {
+        puts("\n|---|---|---|");
+        x++;
+      }
     }
-    if ((x == 1) || (x == 2)){
-      puts("\n|---|---|---|");
-      x++;
+    puts("\n------------");
+
+    puts("\n\nHrací pole");
+
+    printf("\n\nHráč č.1: %s     Hráč č.2: %s                   ", jmeno1, jmeno2);
+    if(aktHrac == 1){
+      printf("Výběr pole: (Hráč č.%d/%s)", aktHrac, jmeno1);
+      scanf("%s", choice);
+      aktHrac = 2;
+    } else {
+      printf("Výběr pole: (Hráč č.%d/%s)", aktHrac, jmeno2);
+      scanf("%s", choice);
+      aktHrac = 1;
     }
   }
-  puts("\n------------");
-  sleep(3);;
   return;
 }
