789b4a86c4d6e007b017a54ef8ae372029acbba2
SimonVesely <simvesely@gmail.com>
2024-10-29 09:45:47 +0100

    Fix leaderboard renderu

--- Linux_MacOS/lib/leaderboard.c
+++ Linux_MacOS/lib/leaderboard.c
@@ -1,8 +1,20 @@
-#include "../include/cJSON.h" // knihovna pro načítání/zapisování souborů typu JSON
 #include "../include/mylib.h" // knihovna s custom scripty, které by se opakovali v částech kódu
-#include <stdio.h>  // knihovna pro načítání vstupů a výstupů
-#include <stdlib.h> // knihovna pro systémové příkazy
-#include <unistd.h> // knihovna pro čas(sleep)
+#include "../include/cJSON.h" // knihovna pro načítání/zapisování souborů typu JSON
+#include <stdio.h>       // knihovna pro načítání vstupů a výstupů
+#include <stdlib.h>      // knihovna pro systémové příkazy
+#include <unistd.h>      // knihovna pro čas(sleep)
+
+int compare_scores(const void *a, const void *b) {
+  cJSON *itemA = *(cJSON **)a;
+  cJSON *itemB = *(cJSON **)b;
+
+  cJSON *scoreA = cJSON_GetObjectItem(itemA, "score");
+  cJSON *scoreB = cJSON_GetObjectItem(itemB, "score");
+
+  return cJSON_IsNumber(scoreB) && cJSON_IsNumber(scoreA)
+             ? (scoreB->valueint - scoreA->valueint)
+             : 0;
+}
 
 void reader_lb() {
 
@@ -21,18 +33,41 @@ void reader_lb() {
   long fileSize = ftell(data);
   fseek(data, 0, SEEK_SET);
   char *buffer = (char *)malloc(fileSize + 1);
+  if (buffer == NULL) {
+    printf("Memory allocation failed\n");
+    fclose(data);
+    return;
+  }
+
   fread(buffer, 1, fileSize, data);
   fclose(data);
   buffer[fileSize] = '\0';
 
   cJSON *json = cJSON_Parse(buffer);
-  free(buffer);
+  //free(buffer);
+
   if (json == NULL) {
-    printf("Error při parsování JSON\n");
+    const char *error_ptr = cJSON_GetErrorPtr();
+    if (error_ptr != NULL && *error_ptr != '\0') {
+      printf("Error při parsování JSON at position: %ld\n", error_ptr - buffer);
+    } else {
+      printf("Unknown JSON parsing error occurred.\n");
+    }
     return;
   }
 
+  // Proceed with processing the JSON object
+  // Proceed with processing the JSON object
   int array_size = cJSON_GetArraySize(json);
+
+  // Vytvoření pointerů pro seřazení pole
+  cJSON **items = malloc(array_size * sizeof(cJSON *));
+  for (int i = 0; i < array_size; i++) {
+    items[i] = cJSON_GetArrayItem(json, i);
+  }
+
+  qsort(items, array_size, sizeof(cJSON *), compare_scores);
+
   for (int i = 0; i < array_size; i++) {
     cJSON *item = cJSON_GetArrayItem(json, i);
 
